c8b3a80b0da558c935d1d09a9e84d5d3695940b5
rodenm18 <rodenmonte@gmail.com>
2016-01-14 15:50:41 -0800

    Corrected the isPrime method, key function.

--- app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
+++ app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
@@ -15,6 +15,14 @@ public class PrimeTester {
      */
     public static boolean isPrime(long n) {
         // for now, return a random result
-        return Math.random() > 0.5;
+        if(n == 0 || n == 1 || n == -1) {
+            return false;
+        }
+        for(int i = 2; i < n; i++){
+            if (n%i == 0){
+                return false;
+            }
+        }
+        return true;
     }
 }
